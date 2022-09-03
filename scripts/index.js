167cb1ec495ba76f5589ac26351c49c99a30cdbd
SizzlinSeal <liamteale6@gmail.com>
2022-09-03 10:44:11 -0700

    reorganized files

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,10 @@
-/**
- * @brief initialize the canvas
- */
-const canvas = document.getElementById('fieldCanvas');
-const canvasQuery = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-const img = new Image();
-img.src = 'images/Top View Render.png';
 let lastPoint = new Point(-2, 0);
 
-/**
- * @brief dev settings
- */
-const imgTrueWidth = 147.8377757;
 
-/**
- * @brief calculate the scale of the canvas
- * To be used in future versions
- */
-const imgActualWidth = Number(canvas.attributes.width.value);
-const imgHalfActualWidth = imgActualWidth / 2;
-const imgPixelsPerInch = imgActualWidth / imgTrueWidth;
-
-
-/**
- * @brief convert the mouse position to position in coordinate system
- * @param {Point} point - the mouse position
- * @return {Point} - the position in the coordinate system
- */
-function pxToCoord(point) {
-  const newPoint = new Point(point.x, point.y);
-  newPoint.x = newPoint.x - imgHalfActualWidth;
-  newPoint.y = imgActualWidth - newPoint.y - imgHalfActualWidth;
-  newPoint.x = newPoint.x / imgPixelsPerInch;
-  newPoint.y = newPoint.y / imgPixelsPerInch;
-  return newPoint;
-};
-
-
-/**
- * @brief convert a point in the coordinate system to the position on the canvas
- * @param {Point} point - the point on the coordinate system
- * @return {Point} - the position on the canvas
- */
-function coordToPx(point) {
-  const newPoint = new Point(point.x, point.y);
-  newPoint.x = newPoint.x * imgPixelsPerInch;
-  newPoint.y = newPoint.y * imgPixelsPerInch;
-  newPoint.x = newPoint.x + imgHalfActualWidth;
-  newPoint.y = imgActualWidth - newPoint.y - imgHalfActualWidth;
-  return newPoint;
-};
+// array containing all the waypoints on the canvas
+let waypoints = [];
+// array containing all the path segments
+let pathSegments = [];
 
 
 /**
--- scripts/point.js
+++ scripts/point.js
@@ -1,3 +1,28 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * @brief initialize the canvas
+ */
+const canvas = document.getElementById('fieldCanvas');
+const canvasQuery = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = 'images/Top View Render.png';
+
+/**
+ * @brief dev settings
+ */
+const imgTrueWidth = 147.8377757;
+
+/**
+ * @brief calculate the scale of the canvas
+ * To be used in future versions
+ */
+const imgActualWidth = Number(canvas.attributes.width.value);
+const imgHalfActualWidth = imgActualWidth / 2;
+const imgPixelsPerInch = imgActualWidth / imgTrueWidth;
+
+
 /**
  * @brief Point that can be used to store coordinates
  */
@@ -12,3 +37,33 @@ class Point {
     this.y = y;
   }
 }
+
+
+/**
+ * @brief convert the mouse position to position in coordinate system
+ * @param {Point} point - the mouse position
+ * @return {Point} - the position in the coordinate system
+ */
+function pxToCoord(point) {
+  const newPoint = new Point(point.x, point.y);
+  newPoint.x = newPoint.x - imgHalfActualWidth;
+  newPoint.y = imgActualWidth - newPoint.y - imgHalfActualWidth;
+  newPoint.x = newPoint.x / imgPixelsPerInch;
+  newPoint.y = newPoint.y / imgPixelsPerInch;
+  return newPoint;
+};
+
+
+/**
+ * @brief convert a point in the coordinate system to the position on the canvas
+ * @param {Point} point - the point on the coordinate system
+ * @return {Point} - the position on the canvas
+ */
+function coordToPx(point) {
+  const newPoint = new Point(point.x, point.y);
+  newPoint.x = newPoint.x * imgPixelsPerInch;
+  newPoint.y = newPoint.y * imgPixelsPerInch;
+  newPoint.x = newPoint.x + imgHalfActualWidth;
+  newPoint.y = imgActualWidth - newPoint.y - imgHalfActualWidth;
+  return newPoint;
+};
