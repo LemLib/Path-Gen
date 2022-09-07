c74c9119fc8149b3a97b52b636f82dd4013b2333
SizzlinSeal <liamteale6@gmail.com>
2022-09-06 20:27:23 -0700

    added temporary control point interpolation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const path = new Path(); // robot path
+
+
 /**
  * @brief function that runs when the window loads
  */
@@ -5,6 +8,17 @@ window.onload = function() {
   // draw image
   ctx.drawImage(img, 0, 0, img.width, img.height, // source rectangle
       0, 0, canvas.width, canvas.height); // destination rectangle
+
+  // add a spline to the path at the start of the path
+  const startP1 = new Point(0, 0);
+  const startP2 = new Point(20, 45);
+  const startP3 = new Point(50, 50);
+  const startP4 = new Point(60, 20);
+  const startSpline = new Spline(startP1, startP2, startP3, startP4);
+  path.addSpline(startSpline);
+
+  // draw the path
+  drawSpline();
 };
 
 
@@ -22,22 +36,31 @@ function getCursorPosition(canvas, event) {
 };
 
 
-let sPoints = [];// array of spline points
-let s; // spline added to path
-let path = new Path(); // robot path
-
 /**
  * @brief when the canvas is left clicked
  * @param {Event} e - the event
  */
 canvasQuery.onclick = function(e) {
-  sPoints.push(pxToCoord(getCursorPosition(canvasQuery, e)));
-  if (sPoints.length == 4) {
-    s = new Spline(sPoints[0], sPoints[1], sPoints[2], sPoints[3]);
-    path.addSpline(s);
-    sPoints = [];
-    drawSpline();
-  }
+  // get the position of the mouse in field coordinates
+  const mousePoint = pxToCoord(getCursorPosition(canvasQuery, e));
+
+  // generate points on the spline
+  // first point, same as the last control point of the last spline
+  const p1 = path.splines[path.splines.length - 1].p4;
+  // second point, same slope as the third control point of the last spline
+  // also has the same magnitude as the third control point of the last spline
+  // and the last point of the last spline
+  const p2CalcVector = new Vector(path.splines[path.splines.length - 1].p3, p1);
+  const p2 = p2CalcVector.interpolate(p2CalcVector.getMagnitude()*2);
+  // third point, just add 5 to the y coordinate of the mouse point
+  const p3 = new Point(mousePoint.x, mousePoint.y + 12);
+  // fourth point, same as the mouse point
+  const p4 = mousePoint;
+
+  // add the spline to the path
+  const spline = new Spline(p1, p2, p3, p4);
+  path.addSpline(spline);
+  drawSpline();
 };
 
 
@@ -56,15 +79,6 @@ canvasQuery.oncontextmenu = function(e) {
  */
 function drawSpline() {
   path.genPoints(10000, (60)*path.splines.length);
-  let message = '';
-
-  // print out spline points coordinates for debug
-  /**
-  for (let i = 0; i < path.points2.length; i++) {
-    message = message + ('(' + path.points2[i].x + ', ' + path.points2[i].y + ')\n');
-  }
-  console.log(message);
-  */
 
   // draw image
   ctx.clearRect(0, 0, canvas.width, canvas.height);
